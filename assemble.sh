0c86eaf3966588882ad9a5b915ac1d11954cd3c9
Bruno Crisafulli <brunocrisafull@gmail.com>
2022-02-02 02:55:42 -0300

    fix typo

--- assemble.sh
+++ assemble.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 #Serving in port 80 without needing to run app with super user permissions
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
-iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 8080
 
 chmod +x mvnw
 ./mvnw install
